bd960d5313a02a4fc1ab8c3743ee06c3c2e6d986
Wojciech Latka <w.latka@itprotech.pl>
2021-04-28 20:44:58 +0200

    Modyfication class js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 console.log("Hello to all developers!")
 
-let js_button = document.querySelector(".js_button");
-let containerBody = document.querySelector(".containerBody");
-let js_table = document.querySelector(".js_table");
-let js_themeName = document.querySelector(".js_themeName");
-let js_newCar = document.querySelector(".js_newCar");
-let js_camper = document.querySelector(".js_camper");
+let button = document.querySelector(".js-button");
+let body = document.querySelector(".js-body");
+let table = document.querySelector(".js-table");
+let themeName = document.querySelector(".js-themeName");
+let newCar = document.querySelector(".js-newCar");
+let camper = document.querySelector(".js-camper");
 
-js_button.addEventListener("click", () => {
-    containerBody.classList.toggle("containerBodydark");
-    js_table.remove();
+button.addEventListener("click", () => {
+    body.classList.toggle("containerBodydark");
+    table.remove();
 
-    if (containerBody.classList.contains("containerBodydark")) {
-        js_themeName.innerText = "jasny";
+    if (body.classList.contains("containerBodydark")) {
+        themeName.innerText = "jasny";
     } else {
-        js_themeName.innerText = "ciemny";
+        themeName.innerText = "ciemny";
     }
 });
 
-js_newCar.addEventListener("click", () => {
-    js_camper.innerHTML = "<img width='70%' src='images/camper.jpg' alt='Nowy Camper' />";
+newCar.addEventListener("click", () => {
+    camper.innerHTML = "<img width='70%' src='images/camper.jpg' alt='Nowy Camper' />";
 });
